33eb654df4eff7841e12e3aa6157eb27c885e275
youngtype <gorodokrege@gmail.com>
2022-02-15 17:23:36 +0200

    comit

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,23 +1,23 @@
-const TableController = require("./tableController");
-const clientModel = require("../models/clientModel");
+const TableController = require('./tableController');
+const clientModel = require('../models/clientModel');
 class ClientController {
-  constructor() {
-    this.tableController = new TableController();
-  }
+	constructor() {
+		this.tableController = new TableController();
+	}
 
-  async create(num) {
-    try {
-      let table = await this.tableController.findByNum(num);
-      let client = await clientModel.create({
-        isWaiting: false,
-        reviews: null,
-        table: table,
-      });
-      return { clientId: client._id.toString() };
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	async create(num) {
+		try {
+			let table = await this.tableController.findByNum(num);
+			let client = await clientModel.create({
+				isWaiting: false,
+				reviews: null,
+				table: table,
+			});
+			return { id: client._id.toString() };
+		} catch (err) {
+			console.log(err);
+		}
+	}
 }
 
 module.exports = ClientController;
